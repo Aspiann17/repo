8151e0b53266950b5ec09d69c9766ff1ab77b808
Aspiann17 <146807024+Aspiann17@users.noreply.github.com>
2023-10-19 11:15:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.discordapp.Discord \
+flatpak install flathub com.spotify.Client \
+flatpak install flathub com.visualstudio.code \
+flatpak install flathub org.telegram.desktop l
+flatpak install flathub com.valvesoftware.Steam \
+flatpak install flathub org.gnome.baobab \
+flatpak install flathub com.getpostman.Postman \
+flatpak install flathub com.github.micahflee.torbrowser-launcher \
