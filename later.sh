aa4db5758fd2e78e101321c1ffaa6aadb6671f7d
Aspiann17 <146807024+Aspiann17@users.noreply.github.com>
2023-11-14 10:55:03 +0800

    Update later.sh

--- later.sh
+++ later.sh
@@ -1,1 +1,5 @@
 sudo apt install nethogs iftop
+
+flatpak install flathub com.protonvpn.www
+flatpak install flathub com.github.micahflee.torbrowser-launcher
+flatpak install flathub com.obsproject.Studio
