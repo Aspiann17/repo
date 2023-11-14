ef13ecd95830cc1f60190e9905bb236aa9b44065
Aspiann17 <146807024+Aspiann17@users.noreply.github.com>
2023-11-14 10:59:48 +0800

    Update later.sh

--- later.sh
+++ later.sh
@@ -3,3 +3,5 @@ sudo apt install nethogs iftop
 flatpak install flathub com.protonvpn.www
 flatpak install flathub com.github.micahflee.torbrowser-launcher
 flatpak install flathub com.obsproject.Studio
+flatpak install flathub io.mrarm.mcpelauncher
+flatpak install flathub cc.arduino.IDE2
